8fa7a388d28697a93fb8937922abf48fa17edb5e
pratikbs2002 <20ce141@charusat.edu.in>
2022-08-31 00:21:19 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import { Home, Page } from './routes'
-// import Page from "./routes/Page";
-import { Switch, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-
+function App() {
   return (
-    <>
-      <div className="container">
-        <Link to="/">Home</Link>
-        <Link to="/Page">Page</Link>
-      </div>
-      <div>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path="/Page" component={Page} />
-        </Switch>
-      </div>
-
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
