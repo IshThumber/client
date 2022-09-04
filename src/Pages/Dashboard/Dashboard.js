eacfdb56b6a210662d95b023bb16bfd5e6166709
pratikbs2002 <20ce141@charusat.edu.in>
2022-09-04 14:39:56 +0530

    login

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "../../App.css";
+import Login from "../../components/Login";
 
 function Dashboard() {
   return (
     <>
       <div className="cr">Dashboard</div>
+      <Login>
+
+      </Login>
     </>
   );
 }
