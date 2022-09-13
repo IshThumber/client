e666a6a26340171a0517fe8d6bda83eae38eb812
Dhruvin Tandel <dhruvintandel0000@gmail.com>
2022-09-13 08:32:54 +0530

    Login CSS

--- src/App.js
+++ src/App.js
@@ -2,45 +2,51 @@ import "./App.css";
 import Sidebar from "./components/Sidebar.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
-import Credentials from "./Pages/CredentialsManagement/CredentialsManagement.js";
 import Dashboard from "./Pages/Dashboard/Dashboard.js";
 import Analytics from "./Pages/Analytics/Analytics.js";
 import StudentManagement from "./Pages/StudentManagement/StudentManagement.js";
+import Credentials from "./Pages/CredentialsManagement/CredentialsManagement.js"
+import Login from "./components/Login.js";
 
 function App() {
   return (
     <>
+
       <Router>
         <div className="Flex">
           <div className="Sidebar-container">
-            <Sidebar /> 
+            <Sidebar />
           </div>
           <div className="Content-container">
-          <Switch>
-            <Route exact path="/">
-              <div className="container">
-                <Dashboard />
-              </div>
-            </Route>
-            <Route exact path="/StudentManagement">
-              <div className="container">
-                <StudentManagement />
-              </div>
-            </Route>
-            <Route exact path="/Analytics">
-              <div className="container">
-                <Analytics />
-              </div>
-            </Route>
-            <Route path="/Credentials">
-              <div className="container">
-                <Credentials />
-              </div>
-            </Route>
-          </Switch>
+            <Switch>
+              <Route exact path="/">
+                <div className="container">
+                  <Dashboard />
+                </div>
+              </Route>
+              <Route exact path="/StudentManagement">
+                <div className="container">
+                  <StudentManagement />
+                </div>
+              </Route>
+              <Route exact path="/Analytics">
+                <div className="container">
+                  <Analytics />
+                </div>
+              </Route>
+              <Route path="/Credentials">
+                <div className="container">
+                  <Credentials />
+                </div>
+              </Route>
+            </Switch>
           </div>
         </div>
       </Router>
+
+      {/* <Login></Login> */}
+
+
     </>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,30 @@
 import React from "react";
 import "./Login.css";
+import pic1 from "./ec1.svg"
+import pic2 from "./ec2.svg"
+import pic3 from "./ec3.svg"
+
 
 function login() {
   return (
     <>
-      <section className="full-screen-container">
-        <div className="clip"></div>
+      <section className="full-screen-container-login">
+        <img className="ec1" src={pic1} alt="Design 1" property="1" />
+        <img className="ec2" src={pic2} alt="Design 2" property="2" />
+        <img className="ec3" src={pic3} alt="Design 3" property="3" />
+
         <div className="sign-in-container">
-          <h1 className="sign-in-title">Sign In</h1>
+          <h1 className="app-name">App Name</h1>
+          <h2 className="login-title">Login</h2>
           <form action="post" className="form">
             <div className="input-container">
-              <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="email" />
+              {/* <label htmlFor="email">Email</label> */}
+              <input type="email" name="email" id="email" placeholder="Id" />
             </div>
 
             <div className="input-container">
-              <label htmlFor="password">Password</label>
-              <input type="email" name="email" id="email" />
+              {/* <label htmlFor="password">Password</label> */}
+              <input type="password" name="password" id="password" placeholder="Password" />
             </div>
             <button type="submit" className="btn-sub">
               Log In
