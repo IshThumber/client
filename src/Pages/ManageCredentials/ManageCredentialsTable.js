6fd311b2444a4ff506704ccbe1a1e6c5890a0aef
K18 <20ce149@charusat.edu.in>
2022-10-06 12:09:33 +0530

    Linked table of teachers to backend

--- src/Pages/ManageCredentials/ManageCredentialsTable.js
+++ src/Pages/ManageCredentials/ManageCredentialsTable.js
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 import { DataGrid, GridActionsCellItem, GridToolbar } from "@mui/x-data-grid";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import TeacherData from "./TeacherData.json";
@@ -8,124 +8,146 @@ import TeacherData from "./TeacherData.json";
 //here id is the key value of id which is shown in the json
 //handler for the edit in class details
 const handleEditClick = (id) => () => {
-  var da = TeacherData.find((std) => std.id === id);
-  alert(`Edit ${da.standard}`);
-  //logic here.............//
+    var da = TeacherData.find((std) => std.id === id);
+    alert(`Edit ${da.standard}`);
+    //logic here.............//
 };
 
 //handler for the delete in class details
 const handleDeleteClick = (id) => () => {
-  var da = TeacherData.find((std) => std.id === id);
-  alert(`Delete ${da.standard}`);
-  //logic here.............//
+    var da = TeacherData.find((std) => std.id === id);
+    alert(`Delete ${da.standard}`);
+    //logic here.............//
 };
 
 //represent columns of table
 const columns = [
-  {
-    field: "standard",
-    headerName: "standard",
-    width: 90,
-    flex: 1,
-    align: "center",
-    headerClassName: "super-app-theme--header",
-    headerAlign: "center",
-  },
-  {
-    field: "userName",
-    align: "center",
-    headerClassName: "super-app-theme--header",
-    headerAlign: "center",
-    headerName: "Username",
-    flex: 1,
-    width: 190,
-  },
-  {
-    field: "password",
-    headerName: "password",
-    width: 90,
-    flex: 1,
-    align: "center",
-    headerClassName: "super-app-theme--header",
-    headerAlign: "center",
-  },
-  {
-    field: "teacherName",
-    align: "center",
-    headerClassName: "super-app-theme--header",
-    headerAlign: "center",
-    headerName: "teacherName",
-    flex: 1,
-    width: 190,
-  },
-  {
-    field: "actions",
-    align: "center",
-    headerClassName: "super-app-theme--header",
-    headerAlign: "center",
-    type: "actions",
-    headerName: "Actions",
-    width: 100,
-    cellClassName: "actions",
-    getActions: ({ id }) => {
-      // const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
-
-      return [
-        <GridActionsCellItem
-          icon={<FaEdit />}
-          label="Edit"
-          className="textPrimary"
-          onClick={handleEditClick(id)}
-          color="inherit"
-        />,
-        <GridActionsCellItem
-          icon={<MdDelete />}
-          label="Delete"
-          onClick={handleDeleteClick(id)}
-          color="inherit"
-        />,
-      ];
+    {
+        field: "standard",
+        headerName: "standard",
+        width: 90,
+        flex: 1,
+        align: "center",
+        headerClassName: "super-app-theme--header",
+        headerAlign: "center",
     },
-  },
+    {
+        field: "teacherId",
+        align: "center",
+        headerClassName: "super-app-theme--header",
+        headerAlign: "center",
+        headerName: "Username",
+        flex: 1,
+        width: 190,
+    },
+    {
+        field: "contactNo",
+        headerName: "Mobile Number",
+        width: 90,
+        flex: 1,
+        align: "center",
+        headerClassName: "super-app-theme--header",
+        headerAlign: "center",
+    },
+    {
+        field: "name",
+        align: "center",
+        headerClassName: "super-app-theme--header",
+        headerAlign: "center",
+        headerName: "teacherName",
+        flex: 1,
+        width: 190,
+    },
+    {
+        field: "actions",
+        align: "center",
+        headerClassName: "super-app-theme--header",
+        headerAlign: "center",
+        type: "actions",
+        headerName: "Actions",
+        width: 100,
+        cellClassName: "actions",
+        getActions: ({ id }) => {
+            // const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+            return [
+                <GridActionsCellItem
+                    icon={<FaEdit />}
+                    label="Edit"
+                    className="textPrimary"
+                    onClick={handleEditClick(id)}
+                    color="inherit"
+                />,
+                <GridActionsCellItem
+                    icon={<MdDelete />}
+                    label="Delete"
+                    onClick={handleDeleteClick(id)}
+                    color="inherit"
+                />,
+            ];
+        },
+    },
 ];
 
 //main function start from here...
 
 export default function ManageCredentialTable(props) {
-  const [row, setRows] = useState(TeacherData); //useState for store rows of table
+    const [row, setRows] = useState([]); //useState for store rows of table
+    const [teacher, setTeacher] = useState(0);
 
-  return (
-    <div>
-      <Box
-        sx={{
-          height: 400,
-          width: "80%",
-          border: "1px solid black",
-          "& .super-app-theme--header": {
-            color: "#ffffff",
-            backgroundColor: "#346f82",
-          },
-        }}
-      >
-        <DataGrid
-          cell--textCenter
-          rows={row}
-          columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5]} //table pagination
-          components={{ Toolbar: GridToolbar }} //header above the table
-          disableColumnFilter //remove filter button
-          disableColumnSelector //to remove column selection
-          disableDensitySelector //to remove density selection
-          disableColumnMenu
-          componentsProps={{
-            toolbar: {
-              showQuickFilter: true,
-              quickFilterProps: { debounceMs: 500 },
-            },
-          }} //search button
-        />
-      </Box>
-    </div>
-  );
+    useEffect(() => {
+        async function getTeacherData() {
+            const teachers = await fetch(
+                `http://localhost:5050/showTeachers/${sessionStorage.getItem(
+                    "schoolId"
+                )}`
+            )
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                })
+                .catch((err) => console.error(err));
+            if (!teachers.message) {
+                setRows(JSON.parse(teachers));
+            } else {
+                setRows([]);
+            }
+        }
+        getTeacherData();
+    }, [teacher]);
+    return (
+        <div>
+            <Box
+                sx={{
+                    height: 400,
+                    width: "80%",
+                    border: "1px solid black",
+                    "& .super-app-theme--header": {
+                        color: "#ffffff",
+                        backgroundColor: "#346f82",
+                    },
+                }}
+            >
+                <DataGrid
+                    cell--textCenter
+                    rows={row}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]} //table pagination
+                    components={{ Toolbar: GridToolbar }} //header above the table
+                    disableColumnFilter //remove filter button
+                    disableColumnSelector //to remove column selection
+                    disableDensitySelector //to remove density selection
+                    disableColumnMenu
+                    componentsProps={{
+                        toolbar: {
+                            showQuickFilter: true,
+                            quickFilterProps: { debounceMs: 500 },
+                        },
+                    }} //search button
+                />
+            </Box>
+        </div>
+    );
 }
