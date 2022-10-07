953ad4a9be1ea250cc32aa8ea9342c440394d5f5
K18 <20ce149@charusat.edu.in>
2022-10-07 06:03:51 +0530

    Linked add student button with API + change in forEach

--- src/Pages/ManageStudents/StudentEntry/StudentEntryForm.js
+++ src/Pages/ManageStudents/StudentEntry/StudentEntryForm.js
@@ -5,276 +5,304 @@ import Controls from "../../../components/controls/Controls";
 import * as employeeService from "../../../SelectItems/SelectItems";
 
 const initialFValues = {
-  id: 0,
-  studentName: "",
-  surName: "",
-  fatherName: "",
-  motherName: "",
-  grNumber: "",
-  uidNumber: "",
-  birthDate: "",
-  mobileNumber: "",
-  caste: "",
+    id: 0,
+    studentName: "",
+    surName: "",
+    fatherName: "",
+    motherName: "",
+    grNumber: "",
+    uidNumber: "",
+    birthDate: "",
+    mobileNumber: "",
+    caste: "",
 };
 
 export default function StudentEntryForm(props) {
-  const validate = () => {
-    let temp = {};
-    // temp.studentName = !/[^a-z]/i.test(values.studentName) ? "This Filed is Required" : "Only Aplphabets are allowed";
+    const validate = () => {
+        let temp = {};
+        // temp.studentName = !/[^a-z]/i.test(values.studentName) ? "This Filed is Required" : "Only Aplphabets are allowed";
 
-    //for student name
-    if (!values.studentName) {
-      temp.studentName = "*Required Field";
-    }
-    else {
-      if (!/[^a-zA-Z]/i.test(values.studentName)) {
-        temp.studentName = '';
-      }
-      else {
-        temp.studentName = 'Only Alaphabets are allowed';
-      }
-    }
-
-    //for surname
-    if (!values.surName) {
-      temp.surName = "*Required Field";
-    }
-    else {
-      if (!/[^a-zA-Z]/i.test(values.surName)) {
-        temp.surName = '';
-      }
-      else {
-        temp.surName = 'Only Alaphabets are allowed';
-      }
-    }
-
-
-    //for father's name
-    if (!values.fatherName) {
-      temp.fatherName = "*Required Field";
-    }
-    else {
-      if (!/[^a-zA-Z]/i.test(values.fatherName)) {
-        temp.fatherName = ''
-      }
-      else {
-        temp.fatherName = 'Only Alaphabets are allowed';
-      }
-    }
-
-
-    // for mother's name
-    if (!values.motherName) {
-      temp.motherName = "*Required Field";
-    }
-    else {
-      if (!/[^a-zA-Z]/i.test(values.motherName)) {
-        temp.motherName = '';
-      }
-      else {
-        temp.motherName = 'Only Alaphabets are allowed';
-      }
-    }
-
-    // grnumber
-    if (!values.grNumber) {
-      temp.grNumber = '*Required Field';
-    }
-    else {
-      temp.grNumber = '';
-    }
-
-    // uid
-    if (!values.uidNumber) {
-      temp.uidNumber = '*Required Field';
-    }
-    else {
-      if ((values.uidNumber.length) < 18) {
-        if ((18 - (values.uidNumber.length)) == 1) {
-          temp.uidNumber = `${18 - (values.uidNumber.length)} digit remain to enter`;
+        //for student name
+        if (!values.studentName) {
+            temp.studentName = "*Required Field";
+        } else {
+            if (!/[^a-zA-Z]/i.test(values.studentName)) {
+                temp.studentName = "";
+            } else {
+                temp.studentName = "Only Alaphabets are allowed";
+            }
         }
-        else {
-          temp.uidNumber = `${18 - (values.uidNumber.length)} digits remain to enter`;
+
+        //for surname
+        if (!values.surName) {
+            temp.surName = "*Required Field";
+        } else {
+            if (!/[^a-zA-Z]/i.test(values.surName)) {
+                temp.surName = "";
+            } else {
+                temp.surName = "Only Alaphabets are allowed";
+            }
         }
-      }
 
-      else if ((values.uidNumber.length) > 18) {
-        if ((values.uidNumber.length - 18) == 1) {
-          temp.uidNumber = `${(values.uidNumber.length) - 18} digit is remain to delete`;
+        //for father's name
+        if (!values.fatherName) {
+            temp.fatherName = "*Required Field";
+        } else {
+            if (!/[^a-zA-Z]/i.test(values.fatherName)) {
+                temp.fatherName = "";
+            } else {
+                temp.fatherName = "Only Alaphabets are allowed";
+            }
         }
-        else {
-          temp.uidNumber = `${(values.uidNumber.length) - 18} digits are remain to delete`;
+
+        // for mother's name
+        if (!values.motherName) {
+            temp.motherName = "*Required Field";
+        } else {
+            if (!/[^a-zA-Z]/i.test(values.motherName)) {
+                temp.motherName = "";
+            } else {
+                temp.motherName = "Only Alaphabets are allowed";
+            }
         }
-      }
-      else {
-        temp.uidNumber = '';
-      }
-    }
 
-
-    //caste
-    if (!values.caste) {
-      temp.caste = '*Required Field'
-    }
-    else {
-      temp.caste = ''
-    }
-
-    //birthdate
-    if (!values.birthDate) {
-      temp.birthDate = '*Required Field'
-    }
-    else {
-      temp.birthDate = ''
-    }
-
-
-    //mobile number
-
-    if (!values.mobileNumber) {
-      temp.mobileNumber = '*Required Field'
-    }
-    else {
-      if (!/[^0-9]/i.test(values.mobileNumber)) {
-        if (values.mobileNumber.length !== 10) {
-          temp.mobileNumber = 'Length must be of 10 numbers'
+        // grnumber
+        if (!values.grNumber) {
+            temp.grNumber = "*Required Field";
+        } else {
+            temp.grNumber = "";
         }
-        else {
-          temp.mobileNumber = ''
+
+        // uid
+        if (!values.uidNumber) {
+            temp.uidNumber = "*Required Field";
+        } else {
+            if (values.uidNumber.length < 18) {
+                if (18 - values.uidNumber.length == 1) {
+                    temp.uidNumber = `${
+                        18 - values.uidNumber.length
+                    } digit remain to enter`;
+                } else {
+                    temp.uidNumber = `${
+                        18 - values.uidNumber.length
+                    } digits remain to enter`;
+                }
+            } else if (values.uidNumber.length > 18) {
+                if (values.uidNumber.length - 18 == 1) {
+                    temp.uidNumber = `${
+                        values.uidNumber.length - 18
+                    } digit is remain to delete`;
+                } else {
+                    temp.uidNumber = `${
+                        values.uidNumber.length - 18
+                    } digits are remain to delete`;
+                }
+            } else {
+                temp.uidNumber = "";
+            }
         }
-      }
-      else {
-        temp.mobileNumber = 'Must be Number'
-      }
-    }
 
-    //birth date
-    if (!values.birthDate) {
-      temp.birthDate = '*Required Field'
-    }
-    else {
-      temp.birthDate = ''
-    }
+        //caste
+        if (!values.caste) {
+            temp.caste = "*Required Field";
+        } else {
+            temp.caste = "";
+        }
 
-    // temp.grNumber = values.grNumber ? "" : "This Filed is Required";
-    // temp.uidNumber = values.uidNumber ? "" : "This Filed is Required";
-    // temp.caste = values.caste ? "" : "This Filed is Required";
-    // temp.birthDate = values.birthDate ? "" : "This Filed is Required";
+        //birthdate
+        if (!values.birthDate) {
+            temp.birthDate = "*Required Field";
+        } else {
+            temp.birthDate = "";
+        }
 
-    setErrors({
-      ...temp,
-    });
-    return Object.values(temp).every((x) => x === "");
-  };
+        //mobile number
 
-  const { values, setValues, errors, setErrors, handleInputChange, resetForm } =
-    useForm(initialFValues);
+        if (!values.mobileNumber) {
+            temp.mobileNumber = "*Required Field";
+        } else {
+            if (!/[^0-9]/i.test(values.mobileNumber)) {
+                if (values.mobileNumber.length !== 10) {
+                    temp.mobileNumber = "Length must be of 10 numbers";
+                } else {
+                    temp.mobileNumber = "";
+                }
+            } else {
+                temp.mobileNumber = "Must be Number";
+            }
+        }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (validate()) {
-      window.alert("Successfully submitted");
-      // employeeService.insertEmployee(values);
-      console.log(values);
-      resetForm();
-    }
-  };
+        //birth date
+        if (!values.birthDate) {
+            temp.birthDate = "*Required Field";
+        } else {
+            temp.birthDate = "";
+        }
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Grid container paddingLeft={20}>
-        <Grid item xs={6}>
-          <Controls.Input
-            name="surName"
-            label="Surname"
-            value={values.surName}
-            onChange={handleInputChange}
-            error={errors.surName}
-          />
+        // temp.grNumber = values.grNumber ? "" : "This Filed is Required";
+        // temp.uidNumber = values.uidNumber ? "" : "This Filed is Required";
+        // temp.caste = values.caste ? "" : "This Filed is Required";
+        // temp.birthDate = values.birthDate ? "" : "This Filed is Required";
 
-          <Controls.Input
-            name="fatherName"
-            label="Father Name"
-            value={values.fatherName}
-            onChange={handleInputChange}
-            error={errors.fatherName}
-          />
+        setErrors({
+            ...temp,
+        });
+        return Object.values(temp).every((x) => x === "");
+    };
 
-          <Controls.Input
-            type="number"
-            name="grNumber"
-            label="G R Number"
-            value={values.grNumber}
-            onChange={handleInputChange}
-            error={errors.grNumber}
-          />
+    const {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange,
+        resetForm,
+    } = useForm(initialFValues);
 
-          <Controls.Input
-            name="mobileNumber"
-            label="Mobile Number"
-            value={values.mobileNumber}
-            onChange={handleInputChange}
-            error={errors.mobileNumber}
-          />
-          <Controls.Select
-            name="caste"
-            label="Caste"
-            value={values.caste}
-            onChange={handleInputChange}
-            options={employeeService.getCasteCollection()}
-            error={errors.caste}
-          ></Controls.Select>
-        </Grid>
-        <Grid item xs={6}>
-          <Controls.Input
-            name="studentName"
-            label="Student Name"
-            value={values.studentName}
-            onChange={handleInputChange}
-            error={errors.studentName}
-          />
-          <Controls.Input
-            name="motherName"
-            label="Mother Name"
-            value={values.motherName}
-            onChange={handleInputChange}
-            error={errors.motherName}
-          />
-          <Controls.Input
-            type='number'
-            name="uidNumber"
-            label="UID Number"
-            value={values.uidNumber}
-            onChange={handleInputChange}
-            error={errors.uidNumber}
-          />
-          <Controls.Input
-            type="date"
-            name="birthDate"
-            value={values.birthDate}
-            onChange={handleInputChange}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">Birth Date :</InputAdornment>
-              ),
-            }}
-            error={errors.birthDate}
-          />
-        </Grid>
-        <Grid container>
-          <Grid item xs={6}></Grid>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (validate()) {
+            window.alert("Successfully submitted");
+            try {
+                const isStudentAdded = await fetch(
+                    "http://localhost:5050/addStudents",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            schoolId: sessionStorage.getItem("schoolId"),
+                            year: new Date().getFullYear(), // Taken Current year by default
+                            studentId: 9, // Must taken form the user
+                            grNo: values.grNumber,
+                            UdiseNo: values.uidNumber,
+                            studentName: values.studentName,
+                            motherName: values.motherName,
+                            fatherName: values.fatherName,
+                            surname: values.surName,
+                            birthDate: values.birthDate,
+                            gender: "Female", // This school is only for girls
+                            caste: values.caste,
+                            standard: "STD 2", // Must taken from the user
+                            address: "ABC", // Must taken from the user
+                            contactNo: values.mobileNumber,
+                            presentCount: 100, // Need discussion on this
+                        }),
+                    }
+                );
+                console.log(isStudentAdded);
+                const parseRes = await isStudentAdded.json();
+                console.log(parseRes);
+            } catch (err) {
+                console.error(err.message);
+            }
+            // employeeService.insertEmployee(values);
+            resetForm();
+        }
+    };
 
-          <Grid item xs={6}>
-            <Controls.Button sx={{ margin: 2 }} type="submit" text="Submit" />
-            <Controls.Button
-              // disabled
-              color="secondary"
-              text="Reset"
-              onClick={resetForm}
-            />
-          </Grid>
-        </Grid>
-      </Grid>
-    </Form>
-  );
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Grid container paddingLeft={20}>
+                <Grid item xs={6}>
+                    <Controls.Input
+                        name="surName"
+                        label="Surname"
+                        value={values.surName}
+                        onChange={handleInputChange}
+                        error={errors.surName}
+                    />
+
+                    <Controls.Input
+                        name="fatherName"
+                        label="Father Name"
+                        value={values.fatherName}
+                        onChange={handleInputChange}
+                        error={errors.fatherName}
+                    />
+
+                    <Controls.Input
+                        type="number"
+                        name="grNumber"
+                        label="G R Number"
+                        value={values.grNumber}
+                        onChange={handleInputChange}
+                        error={errors.grNumber}
+                    />
+
+                    <Controls.Input
+                        name="mobileNumber"
+                        label="Mobile Number"
+                        value={values.mobileNumber}
+                        onChange={handleInputChange}
+                        error={errors.mobileNumber}
+                    />
+                    <Controls.Select
+                        name="caste"
+                        label="Caste"
+                        value={values.caste}
+                        onChange={handleInputChange}
+                        options={employeeService.getCasteCollection()}
+                        error={errors.caste}
+                    ></Controls.Select>
+                </Grid>
+                <Grid item xs={6}>
+                    <Controls.Input
+                        name="studentName"
+                        label="Student Name"
+                        value={values.studentName}
+                        onChange={handleInputChange}
+                        error={errors.studentName}
+                    />
+                    <Controls.Input
+                        name="motherName"
+                        label="Mother Name"
+                        value={values.motherName}
+                        onChange={handleInputChange}
+                        error={errors.motherName}
+                    />
+                    <Controls.Input
+                        type="number"
+                        name="uidNumber"
+                        label="UID Number"
+                        value={values.uidNumber}
+                        onChange={handleInputChange}
+                        error={errors.uidNumber}
+                    />
+                    <Controls.Input
+                        type="date"
+                        name="birthDate"
+                        value={values.birthDate}
+                        onChange={handleInputChange}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    Birth Date :
+                                </InputAdornment>
+                            ),
+                        }}
+                        error={errors.birthDate}
+                    />
+                </Grid>
+                <Grid container>
+                    <Grid item xs={6}></Grid>
+
+                    <Grid item xs={6}>
+                        <Controls.Button
+                            sx={{ margin: 2 }}
+                            type="submit"
+                            text="Submit"
+                        />
+                        <Controls.Button
+                            // disabled
+                            color="secondary"
+                            text="Reset"
+                            onClick={resetForm}
+                        />
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Form>
+    );
 }
