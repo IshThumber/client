f06eca490f01fa7f07d28ce1b8c794fec172899e
Ish Thumber <80597098+IshThumber@users.noreply.github.com>
2022-10-20 00:13:57 +0530

    Dashboard table viewing status

--- src/Pages/Dashboard/DashboardStatusTable.js
+++ src/Pages/Dashboard/DashboardStatusTable.js
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import React, { useEffect, useState } from "react";
-import StatusData from "./subjectStatus.json";
+// import StatusData from "./subjectStatus.json";
 
 //represent columns of table
 const columns1 = [
@@ -12,25 +12,25 @@ const columns1 = [
     flex: 1,
     align: "center",
     headerClassName: "super-app-theme--header",
-    headerAlign: "center",
+    headerAlign: "center"
   },
   {
-    field: "gujarati",
+    field: "Gujarati",
     align: "center",
     headerClassName: "super-app-theme--header",
     headerAlign: "center",
     headerName: "Gujarati",
     flex: 1,
-    width: 190,
+    width: 190
   },
   {
-    field: "maths",
-    headerName: "maths",
+    field: "English",
+    headerName: "English",
     width: 90,
     flex: 1,
     align: "center",
     headerClassName: "super-app-theme--header",
-    headerAlign: "center",
+    headerAlign: "center"
   },
   {
     field: "hindi",
@@ -39,8 +39,8 @@ const columns1 = [
     headerAlign: "center",
     headerName: "hindi",
     flex: 1,
-    width: 190,
-  },
+    width: 190
+  }
 ];
 const columns2 = [
   {
@@ -50,7 +50,7 @@ const columns2 = [
     flex: 1,
     align: "center",
     headerClassName: "super-app-theme--header",
-    headerAlign: "center",
+    headerAlign: "center"
   },
   {
     field: "gujarati",
@@ -59,7 +59,7 @@ const columns2 = [
     headerAlign: "center",
     headerName: "Gujarati",
     flex: 1,
-    width: 190,
+    width: 190
   },
   {
     field: "maths",
@@ -68,7 +68,7 @@ const columns2 = [
     flex: 1,
     align: "center",
     headerClassName: "super-app-theme--header",
-    headerAlign: "center",
+    headerAlign: "center"
   },
   {
     field: "hindi",
@@ -77,19 +77,72 @@ const columns2 = [
     headerAlign: "center",
     headerName: "hindi",
     flex: 1,
-    width: 190,
-  },
+    width: 190
+  }
 ];
 export default function DashboardStatusTable() {
-  const [row1, setRows1] = useState(StatusData); //useState for store rows of table
-  const [row2, setRows2] = useState(StatusData); //useState for store rows of table
-  useEffect(() => {
-    let ob1 = StatusData.filter((d) => d.standard <= 5);
-    let ob2 = StatusData.filter((d) => d.standard > 5);
-    setRows1(ob1);
-    setRows2(ob2);
-  }, []);
+  const [row1, setRows1] = useState(0); //useState for store rows of table
+  const [row2, setRows2] = useState(0); //useState for store rows of table
 
+  // async function statusData() {
+
+  // let year = 2022;
+  // let sem = semester.toString();
+  // let status = await fetch(
+  //   `http://localhost:5050/dashboard/${sessionStorage.getItem("schoolId")}/${year}/${sem}`,
+  // )
+  //   .then((res) => {
+  //     if (res.ok) {
+  //       return res.json();
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
+  // console.log(status);
+  // console.log("status");
+  // }
+
+  const [semester, setSemester] = useState(2);
+
+  useEffect(() => {
+    // let ob1 = StatusData.filter((d) => d.standard <= 5);
+    // let ob2 = StatusData.filter((d) => d.standard > 5);
+    // setRows1(ob1);
+    // setRows2(ob2);
+    // console.log("Hiiiiii");
+    async function statusData() {
+      console.log("Hi");
+      let year = 2022;
+      let sem = semester.toString();
+      const status = await fetch(
+        `http://localhost:5050/dashboard/${sessionStorage.getItem(
+          "schoolId"
+        )}/${year}/${sem}`
+      )
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+      console.log(status);
+
+      if (status) {
+        let ob1 = status.filter((d) => parseInt(d.standard.substring(4,5)) <= 5);
+        let ob2 = status.filter((d) => parseInt(d.standard.substring(4,5)) > 5);
+        setRows1(ob1);
+        setRows2(ob2);
+      }
+
+    }
+    statusData();
+  }, []);
+  // statusData();
   return (
     <div>
       <Box
@@ -99,8 +152,8 @@ export default function DashboardStatusTable() {
           border: "1px solid black",
           "& .super-app-theme--header": {
             color: "#ffffff",
-            backgroundColor: "#346f82",
-          },
+            backgroundColor: "#346f82"
+          }
         }}
       >
         <DataGrid
@@ -123,14 +176,14 @@ export default function DashboardStatusTable() {
 
       <Box
         sx={{
-          marginTop : 2,
+          marginTop: 2,
           height: 300,
           width: "80%",
           border: "1px solid black",
           "& .super-app-theme--header": {
             color: "#ffffff",
-            backgroundColor: "#346f82",
-          },
+            backgroundColor: "#346f82"
+          }
         }}
       >
         <DataGrid
