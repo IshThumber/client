3e2c61eca288b8b4f0c115bad6a236aa265b011e
pratikbs2002 <20ce141@charusat.edu.in>
2022-10-22 11:14:49 +0530

    suject list

--- src/Pages/Dashboard/DashboardStatusTable.js
+++ src/Pages/Dashboard/DashboardStatusTable.js
@@ -7,215 +7,269 @@ import "./Dashboard.css";
 
 //represent columns of table
 const columns1 = [
-    {
-        field: "standard",
-        headerName: "standard",
-        width: 90,
-        flex: 1,
-        align: "center",
-        headerClassName: "super-app-theme--header",
-        headerAlign: "center",
-    },
-    {
-        field: "Gujarati",
-        align: "center",
-        headerClassName: "super-app-theme--header",
-        headerAlign: "center",
-        headerName: "Gujarati",
-        flex: 1,
-        width: 190,
-    },
-    {
-        field: "English",
-        headerName: "English",
-        width: 90,
-        flex: 1,
-        align: "center",
-        headerClassName: "super-app-theme--header",
-        headerAlign: "center",
-    },
-    {
-        field: "hindi",
-        align: "center",
-        headerClassName: "super-app-theme--header",
-        headerAlign: "center",
-        headerName: "hindi",
-        flex: 1,
-        width: 190,
-    },
+  {
+    field: "standard",
+    headerName: "Standard",
+    width: 90,
+    flex: 1,
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+  },
+  {
+    field: "Gujarati",
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    headerName: "Gujarati",
+    flex: 1,
+    width: 190,
+  },
+  {
+    field: "English",
+    headerName: "English",
+    width: 90,
+    flex: 1,
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+  },
+  {
+    field: "Hindi",
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    headerName: "Hindi",
+    flex: 1,
+    width: 190,
+  },
+  {
+    field: "Maths",
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    headerName: "Maths",
+    flex: 1,
+    width: 190,
+  },
+  {
+    field: "Environment Study",
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    headerName: "Environment Study",
+    flex: 1,
+    width: 190,
+  },
 ];
 const columns2 = [
-    {
-        field: "standard",
-        headerName: "standard",
-        width: 90,
-        flex: 1,
-        align: "center",
-        headerClassName: "super-app-theme--header",
-        headerAlign: "center",
-    },
-    {
-        field: "gujarati",
-        align: "center",
-        headerClassName: "super-app-theme--header",
-        headerAlign: "center",
-        headerName: "Gujarati",
-        flex: 1,
-        width: 190,
-    },
-    {
-        field: "maths",
-        headerName: "maths",
-        width: 90,
-        flex: 1,
-        align: "center",
-        headerClassName: "super-app-theme--header",
-        headerAlign: "center",
-    },
-    {
-        field: "hindi",
-        align: "center",
-        headerClassName: "super-app-theme--header",
-        headerAlign: "center",
-        headerName: "hindi",
-        flex: 1,
-        width: 190,
-    },
+  {
+    field: "standard",
+    headerName: "Standard",
+    width: 90,
+    flex: 1,
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+  },
+  {
+    field: "Gujarati",
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    headerName: "Gujarati",
+    flex: 1,
+    width: 190,
+  },
+  {
+    field: "Maths",
+    headerName: "Maths",
+    width: 90,
+    flex: 1,
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+  },
+  {
+    field: "Hindi",
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    headerName: "Hindi",
+    flex: 1,
+    width: 190,
+  },
+  {
+    field: "English",
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    headerName: "English",
+    flex: 1,
+    width: 190,
+  },
+  {
+    field: "Science",
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    headerName: "Science",
+    flex: 1,
+    width: 190,
+  },
+  {
+    field: "Social Science",
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    headerName: "Social Science",
+    flex: 1,
+    width: 190,
+  },
+  {
+    field: "Sanskrit",
+    align: "center",
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    headerName: "Sanskrit",
+    flex: 1,
+    width: 190,
+  },
 ];
 export default function DashboardStatusTable() {
-    const [row1, setRows1] = useState([]); //useState for store rows of table
-    const [row2, setRows2] = useState([]); //useState for store rows of table
-    const [isRowFetched, setIsRowFetched] = useState(true);
-    // async function statusData() {
+  const [row1, setRows1] = useState([]); //useState for store rows of table
+  const [row2, setRows2] = useState([]); //useState for store rows of table
+  const [isRowFetched, setIsRowFetched] = useState(true);
+  // async function statusData() {
 
-    // let year = 2022;
-    // let sem = semester.toString();
-    // let status = await fetch(
-    //   `http://localhost:5050/dashboard/${sessionStorage.getItem("schoolId")}/${year}/${sem}`,
-    // )
-    //   .then((res) => {
-    //     if (res.ok) {
-    //       return res.json();
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
+  // let year = 2022;
+  // let sem = semester.toString();
+  // let status = await fetch(
+  //   `http://localhost:5050/dashboard/${sessionStorage.getItem("schoolId")}/${year}/${sem}`,
+  // )
+  //   .then((res) => {
+  //     if (res.ok) {
+  //       return res.json();
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 
-    // console.log(status);
-    // console.log("status");
-    // }
+  // console.log(status);
+  // console.log("status");
+  // }
 
-    const [semester, setSemester] = useState(2);
+  const [semester, setSemester] = useState(2);
 
-    useEffect(() => {
-        // let ob1 = StatusData.filter((d) => d.standard <= 5);
-        // let ob2 = StatusData.filter((d) => d.standard > 5);
-        // setRows1(ob1);
-        // setRows2(ob2);
-        // console.log("Hiiiiii");
-        setIsRowFetched(false);
-        async function statusData() {
-            console.log("Hi");
-            let year = 2022;
-            let sem = semester.toString();
-            const status = await fetch(
-                `http://localhost:5050/dashboard/${sessionStorage.getItem(
-                    "schoolId"
-                )}/${year}/${sem}`
-            )
-                .then((res) => {
-                    if (res.ok) {
-                        return res.json();
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
+  useEffect(() => {
+    // let ob1 = StatusData.filter((d) => d.standard <= 5);
+    // let ob2 = StatusData.filter((d) => d.standard > 5);
+    // setRows1(ob1);
+    // setRows2(ob2);
+    // console.log("Hiiiiii");
+    setIsRowFetched(false);
+    async function statusData() {
+      console.log("Hi");
+      let year = 2022;
+      let sem = semester.toString();
+      const status = await fetch(
+        `http://localhost:5050/dashboard/${sessionStorage.getItem(
+          "schoolId"
+        )}/${year}/${sem}`
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
 
-            console.log(status);
+      console.log(status);
 
-            if (status) {
-                let ob1 = status.filter(
-                    (d) => parseInt(d.standard.substring(4, 5)) <= 5
-                );
-                let ob2 = status.filter(
-                    (d) => parseInt(d.standard.substring(4, 5)) > 5
-                );
-                setRows1(ob1);
-                setRows2(ob2);
-                setIsRowFetched(true);
-            }
-        }
-        statusData();
-    }, []);
-    // statusData();
-    return (
-        <>
-            <div>
-                {isRowFetched ? (
-                    <>
-                        <Box
-                            sx={{
-                                height: 300,
-                                width: "80%",
-                                border: "1px solid black",
-                                "& .super-app-theme--header": {
-                                    color: "#ffffff",
-                                    backgroundColor: "#346f82",
-                                },
-                            }}
-                        >
-                            <DataGrid
-                                cell--textCenter
-                                rows={row1}
-                                columns={columns1}
-                                pageSize={5}
-                                // rowsPerPageOptions={[5]} //table pagination
-                                components={{ Toolbar: GridToolbar }} //header above the table
-                                disableColumnFilter //remove filter button
-                                disableColumnSelector //to remove column selection
-                                disableDensitySelector //to remove density selection
-                                disableColumnMen
-                                disablrPagination
-                                disableSelectionOnClick
-                                disableVirtualization
-                                disableExtendRowFullWidth
-                            />
-                        </Box>
+      if (status) {
+        let ob1 = status.filter(
+          (d) => parseInt(d.standard.substring(4, 5)) <= 5
+        );
+        let ob2 = status.filter(
+          (d) => parseInt(d.standard.substring(4, 5)) > 5
+        );
+        setRows1(ob1);
+        setRows2(ob2);
+        setIsRowFetched(true);
+      }
+    }
+    statusData();
+  }, []);
+  // statusData();
+  return (
+    <>
+      <div>
+        {isRowFetched ? (
+          <>
+            <Box
+              sx={{
+                height: 300,
+                width: "80%",
+                border: "1px solid black",
+                "& .super-app-theme--header": {
+                  color: "#ffffff",
+                  backgroundColor: "#346f82",
+                },
+              }}
+            >
+              <DataGrid
+                cell--textCenter
+                rows={row1}
+                columns={columns1}
+                pageSize={5}
+                // rowsPerPageOptions={[5]} //table pagination
+                components={{ Toolbar: GridToolbar }} //header above the table
+                disableColumnFilter //remove filter button
+                disableColumnSelector //to remove column selection
+                disableDensitySelector //to remove density selection
+                disableColumnMen
+                disablrPagination
+                disableSelectionOnClick
+                disableVirtualization
+                disableExtendRowFullWidth
+              />
+            </Box>
 
-                        <Box
-                            sx={{
-                                marginTop: 2,
-                                height: 300,
-                                width: "80%",
-                                border: "1px solid black",
-                                "& .super-app-theme--header": {
-                                    color: "#ffffff",
-                                    backgroundColor: "#346f82",
-                                },
-                            }}
-                        >
-                            <DataGrid
-                                cell--textCenter
-                                rows={row2}
-                                columns={columns2}
-                                pageSize={8}
-                                // rowsPerPageOptions={[5]} //table pagination
-                                components={{ Toolbar: GridToolbar }} //header above the table
-                                disableColumnFilter //remove filter button
-                                disableColumnSelector //to remove column selection
-                                disableDensitySelector //to remove density selection
-                                disableColumnMen
-                            />
-                        </Box>
-                    </>
-                ) : (
-                    <div className="admin-loader-class">
-                        <Loader />
-                    </div>
-                )}
-            </div>
-        </>
-    );
+            <Box
+              sx={{
+                marginTop: 2,
+                height: 300,
+                width: "80%",
+                border: "1px solid black",
+                "& .super-app-theme--header": {
+                  color: "#ffffff",
+                  backgroundColor: "#346f82",
+                },
+              }}
+            >
+              <DataGrid
+                cell--textCenter
+                rows={row2}
+                columns={columns2}
+                pageSize={8}
+                // rowsPerPageOptions={[5]} //table pagination
+                components={{ Toolbar: GridToolbar }} //header above the table
+                disableColumnFilter //remove filter button
+                disableColumnSelector //to remove column selection
+                disableDensitySelector //to remove density selection
+                disableColumnMen
+              />
+            </Box>
+          </>
+        ) : (
+          <div className="admin-loader-class">
+            <Loader />
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
